f2880cb5eab4a4092d61acd9f138f402ab1b5fce
pluizetto <41959558+pluizetto@users.noreply.github.com>
2024-06-06 12:09:41 -0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  if ENV['Decoder_Ring']
-    "<p>Decoder_Ring: #{ENV['Decoder_Ring']}</p><p style='color:green;'>Success</p>"
+  if ENV['CICLOP']
+    "<p>CICLOP: #{ENV['CICLOP']}</p><p style='color:green;'>Success</p>"
   else
     "<p style='color:red;'>Failed, please, check your configuration!</p>"
   end
